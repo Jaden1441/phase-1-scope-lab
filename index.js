dc20b3eae908bc9510c270db7601a5452b6a9511
Jaden <jaden.franks@gmail.com>
2024-06-13 19:31:13 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName(){
+    return customerName = customerName.toUpperCase();
+}
+
+//var bestCustomer = 'jay';
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+    return bestCustomer;
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'dad'
+function changeLeastFavoriteCustomer(){
+    return leastFavoriteCustomer = 'mom'
+}
